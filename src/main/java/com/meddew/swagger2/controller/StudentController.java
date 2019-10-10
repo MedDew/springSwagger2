8701d7552184e3053359d07d1a3d88ea6f17f5f4
Med <Med>
2019-10-10 10:57:47 +0200

    add RepositoryRestResource annotation

---
+++ src/main/java/com/meddew/swagger2/controller/StudentController.java
@@ -0,0 +1,4 @@
+package com.meddew.swagger2.controller;
+
+public class StudentController {
+}
--- src/main/java/com/meddew/swagger2/repositories/StudentRepostory.java
+++ src/main/java/com/meddew/swagger2/repositories/StudentRepostory.java
@@ -2,6 +2,8 @@ package com.meddew.swagger2.repositories;
 
 import com.meddew.swagger2.entities.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource
 public interface StudentRepostory extends CrudRepository<Student, Long> {
 }
