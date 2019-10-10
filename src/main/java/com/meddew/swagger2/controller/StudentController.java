be42dcdac5c4e6079392ee02f7e3264b11bc4f51
Med <Med>
2019-10-10 10:58:56 +0200

    add a controller to be able to view the documented enpoint by swagger 2

--- src/main/java/com/meddew/swagger2/controller/StudentController.java
+++ src/main/java/com/meddew/swagger2/controller/StudentController.java
@@ -1,4 +1,26 @@
 package com.meddew.swagger2.controller;
 
+import com.meddew.swagger2.entities.Student;
+import com.meddew.swagger2.repositories.StudentRepostory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class StudentController {
+    @Autowired
+    private StudentRepostory studentRepostory;
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> studentList = new ArrayList<>();
+        studentRepostory.findAll().forEach( s -> {
+            studentList.add(s);
+        });
+
+        return studentList;
+    }
 }
