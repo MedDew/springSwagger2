d137ce8ca259a4810fe7d21da09b6adb667c60b7
Med <Med>
2019-10-10 14:54:51 +0200

    add annotation in charge of describing the role of the endpoint

--- src/main/java/com/meddew/swagger2/controller/StudentController.java
+++ src/main/java/com/meddew/swagger2/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.meddew.swagger2.controller;
 
 import com.meddew.swagger2.entities.Student;
 import com.meddew.swagger2.repositories.StudentRepostory;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class StudentController {
     private StudentRepostory studentRepostory;
 
     @GetMapping("/students")
+    @ApiOperation(value = "Fetch all the students", notes = "Return the complete list of students",response = Student.class)
     public List<Student> getStudents(){
         List<Student> studentList = new ArrayList<>();
         studentRepostory.findAll().forEach( s -> {
