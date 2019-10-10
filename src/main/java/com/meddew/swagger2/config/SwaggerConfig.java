5a840afae2a9af254c3a6b39c885e6059d8e4175
Med <Med>
2019-10-10 11:51:35 +0200

    override message description for http status 500 and 403

--- src/main/java/com/meddew/swagger2/config/SwaggerConfig.java
+++ src/main/java/com/meddew/swagger2/config/SwaggerConfig.java
@@ -2,21 +2,41 @@ package com.meddew.swagger2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api(){
+
+        List<ResponseMessage> responseMessages = Arrays.asList(
+                        new ResponseMessageBuilder()
+                        .code(500)
+                        .message("Internal Server Error")
+                        .responseModel(new ModelRef("Error 500"))
+                        .build(),
+                        new ResponseMessageBuilder()
+                        .code(403)
+                        .message("Forbidden!!")
+                        .build()
+        );
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 //To be able to see each documented route in the swagger limited to the scope of my API
@@ -28,7 +48,11 @@ public class SwaggerConfig {
                 //To see the swagger documentation limited to base package given : see the StudentController
                 .paths(PathSelectors.ant("/school/*"))
                 .build()
-                .apiInfo(getApiDetails());
+                .apiInfo(getApiDetails())
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET,
+                    new ArrayList<ResponseMessage>(responseMessages)
+                );
     }
 
     private ApiInfo getApiDetails(){
