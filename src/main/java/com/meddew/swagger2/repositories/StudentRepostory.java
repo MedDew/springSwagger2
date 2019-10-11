d3e98ec28e2a94dde7dd7579154c5a69fde422bf
Med <Med>
2019-10-11 09:24:36 +0200

    Add a RepositoryRestResource to enable api acting as HATEOAS

--- src/main/java/com/meddew/swagger2/repositories/StudentRepostory.java
+++ src/main/java/com/meddew/swagger2/repositories/StudentRepostory.java
@@ -4,6 +4,6 @@ import com.meddew.swagger2.entities.Student;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//@RepositoryRestResource
+@RepositoryRestResource
 public interface StudentRepostory extends CrudRepository<Student, Long> {
 }
